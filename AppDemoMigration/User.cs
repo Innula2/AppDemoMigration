83b464e2e4c929a49b98136dd672b54d064e5b40
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-03-10 15:24:56 +0300

    Добавьте файлы проекта.

---
+++ AppDemoMigration/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppDemoMigration
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public bool IsMarried { get; set; }
+    }
+}
